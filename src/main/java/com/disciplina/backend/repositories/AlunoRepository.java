de06cd2f87d627efeeb7097a2e4f718d6004e431
André Enedino <andreenedinosantos@gmail.com>
2022-09-14 19:21:57 -0300

    Primeiro commit

---
+++ src/main/java/com/disciplina/backend/repositories/AlunoRepository.java
@@ -0,0 +1,9 @@
+package com.disciplina.backend.repositories;
+
+import com.disciplina.backend.models.Aluno;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AlunoRepository extends JpaRepository<Aluno, Long> {
+}
---
+++ src/main/java/com/disciplina/backend/service/AlunoService.java
@@ -0,0 +1,18 @@
+package com.disciplina.backend.service;
+
+import com.disciplina.backend.models.Aluno;
+import com.disciplina.backend.repositories.AlunoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AlunoService {
+
+    @Autowired
+    AlunoRepository repository;
+
+    public Aluno create(Aluno aluno){
+
+        return repository.save(aluno);
+    }
+}
